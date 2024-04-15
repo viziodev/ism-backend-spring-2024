c26bf87f490b063b02f98f2510326d33dfcf6ccd
douvewane <douvewane85@gmail.com>
2024-04-15 03:41:18 +0000

    Rest Api

--- src/main/java/com/ism/ecom/services/ArticleService.java
+++
@@ -1,9 +0,0 @@
-package com.ism.ecom.services;
-
-import com.ism.ecom.data.entities.Article;
-
-import java.util.List;
-
-public interface ArticleService {
-    public List<Article> getArticlesFormComande();
-}
